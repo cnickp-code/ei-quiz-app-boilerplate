b62790a4614f232d981c4b18472260ec6aa3d89a
Carlo Paredes <c.nick.paredes@gmail.com>
2020-04-23 15:16:01 -0700

    Added HTML structure arrays

--- app.js
+++ app.js
@@ -48,6 +48,75 @@ const store = {
 /********** TEMPLATE GENERATION FUNCTIONS **********/
 
 // These functions return HTML templates
+function generateQuiz(item) {
+  let quizStructure = [
+    '<header>',
+      '<h1>How Well Do You Know Video Games?</h1>',
+    '</header>',
+    '<main>',
+      '<div class="score">Results go here O X O</div>',
+      '<div class="question">Question</div>',
+      '<form>',
+        '<fieldset class="quiz-container">',
+          '<input type="radio" name="quizanswer" class="answers" id="quiz-ans-1" value="0" checked>',
+          '<label for="quiz-ans-1">Answer 1</label>',
+          '<br>',
+          '<input type="radio" name="quizanswer" class="answers" id="quiz-ans-2" value="1">',
+          '<label for="quiz-ans-2">Answer 2</label>',
+          '<br>',
+          '<input type="radio" name="quizanswer" class="answers" id="quiz-ans-3" value="2">',
+          '<label for="quiz-ans-3">Answer 3</label>',
+          '<br>',
+          '<input type="radio" name="quizanswer" class="answers" id="quiz-ans-2" value="3">',
+          '<label for="quiz-ans-2">Answer 4</label>',
+          '<br>',
+          '<button type="submit" class ="submit-button next-button">Submit</button>',
+        '</fieldset>',
+      '</form>',
+      
+    '</main>'
+  ];
+
+  return quizStructure;
+}
+
+function generateStartPage(item) {
+  let startStructure = [
+    '<header>',
+      '<h1>How Well Do You Know Video Games?</h1>',
+    '</header>',
+    '<main>',
+      '<form>',
+        '<fieldset class="start-container">',
+          '<div class="instructions">Instructions:</div>',
+            '<div class="instructions-body">Placeholder text for instructions</div>',
+         '<button type="submit" class ="submit-button start">Start Game</button>',
+        '</fieldset>',
+      '</form>',
+    '</main>'
+  ];
+
+  return startStructure;
+}
+
+function generateEndPage(item) {
+  let endStructure = [
+    '<header>',
+      '<h1>How Well Do You Know Video Games?</h1>',
+    '</header>',
+    '<main>',
+      '<form>',
+        '<fieldset class="end-container">',
+          '<div class="results">Results:</div>',
+            '<div class="results-body">Placeholder text for results</div>',
+          '<button type="submit" class ="submit-button start-button">Play Again</button>',
+        '</fieldset>',
+      '</form>',
+    '</main>'
+  ];
+}
+
+
 
 /********** RENDER FUNCTION(S) **********/
 
